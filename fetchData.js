8df02b982f2e3ea090c367a007950457a4cd576e
Johnny <Jonathan.Peckover@academytrainee.com>
2019-06-07 10:40:56 +0100

    added fetchData.js with getAllOwners functionality

---
+++ fetchData.js
@@ -0,0 +1,26 @@
+function fetchData(url, method, body) {
+
+    return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.onreadystatechange = () => {
+            if (req.readyState === 4) {
+                if (((req.status >= 200) && (req.status <= 299))) {
+                    resolve(req);
+                } else {
+                    reject("Name does not match existing records");
+                }
+            }
+        };
+        req.open(method, url);
+        req.setRequestHeader("Content-Type", "application/json");
+        req.send(JSON.stringify(body));
+    });
+}
+
+
+function getOWners() {
+    return fetchData(`http://localhost:9966/petclinic/api/owners`, 'GET')
+        .then((req) => {
+            return JSON.parse(req.responseText);
+        });
+}
